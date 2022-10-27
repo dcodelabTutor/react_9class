2f8e861ec1f2f6256e91827771a5ac6d13c2f5a6
tubi55 <tubi55@nate.com>
2022-10-27 12:16:15 +0900

    메인 컴포넌트인 Vids에서 store에 있는 전역데이터로 스와이프 슬라이드 동적 구현

--- src/components/main/Vids.js
+++ src/components/main/Vids.js
@@ -3,32 +3,40 @@ import { Pagination, Navigation } from 'swiper';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
+import { useSelector } from 'react-redux';
 
 function Vids() {
-    return (
-        <main id="vids" className='myScroll'>
-            <Swiper
-                modules={[Pagination, Navigation]}
-                pagination={
-                    {
-                        clickable: true,
-                    }
-                }
-                spaceBetween={60}
-                navigation={true}
-                loop={true}
-                slidesPerView={3}
-                centeredSlides={true}
-            >
-                <SwiperSlide>
-                    <div className="inner">1</div>
-                </SwiperSlide>
-                <SwiperSlide><div className="inner">2</div></SwiperSlide>
-                <SwiperSlide><div className="inner">3</div></SwiperSlide>
-                <SwiperSlide><div className="inner">4</div></SwiperSlide>
-                <SwiperSlide><div className="inner">5</div></SwiperSlide>
-            </Swiper>
-        </main>
-    );
+	const { youtube } = useSelector((store) => store.youtubeReducer);
+	console.log(youtube)
+
+	return (
+		<main id="vids" className='myScroll'>
+			<Swiper
+				modules={[Pagination, Navigation]}
+				pagination={
+					{
+						clickable: true,
+					}
+				}
+				spaceBetween={60}
+				navigation={true}
+				loop={true}
+				slidesPerView={3}
+				centeredSlides={true}
+			>
+				{youtube.map((vid, idx) => {
+					return (
+						<SwiperSlide key={idx}>
+							<div className="inner">
+								<div className="pic">
+									<img src={vid.snippet.thumbnails.standard.url} />
+								</div>
+							</div>
+						</SwiperSlide>
+					)
+				})}
+			</Swiper>
+		</main>
+	);
 }
 export default Vids;
--- src/components/sub/Youtube.js
+++ src/components/sub/Youtube.js
@@ -57,8 +57,6 @@ export default function Youtube() {
 						</article>
 					);
 				})}
-
-
 			</Layout>
 
 			<Popup ref={pop}>
